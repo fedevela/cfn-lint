28ce56f561d6209e42c97404267e0ecfdee603c9
Federico Vela <fedevela@gmail.com>
2026-07-17 16:02:16 -0500

    phase 10 : Malkhut

--- test/unit/rules/resources/iam/test_managed_policy_size.py
+++ test/unit/rules/resources/iam/test_managed_policy_size.py
@@ -3,6 +3,8 @@ Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 SPDX-License-Identifier: MIT-0
 """
 
+import json
+
 from cfnlint.rules.resources.properties.Properties import Properties
 from cfnlint.rules.resources.properties.StringLength import StringLength
 
@@ -38,6 +40,36 @@ def test_iammp_001_static_managed_policy_compact_over_6144_reports_policy_doc_er
 
 
 def test_iammp_002_validating_compact_managed_policy_at_6144_does_not_report_size_error(
+    validator,
 ):
     """IAMMP-002: an exact-limit compact PolicyDocument has no size-limit error."""
-    assert True
+    rule = Properties()
+    rule.child_rules["E3033"] = StringLength()
+    policy_document = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": "service:Action",
+                "Resource": "",
+            }
+        ],
+    }
+    compact_length = len(json.dumps(policy_document, separators=(",", ":")))
+    policy_document["Statement"][0]["Resource"] = "a" * (6144 - compact_length)
+    resource = {
+        "Type": "AWS::IAM::ManagedPolicy",
+        "Properties": {"PolicyDocument": policy_document},
+    }
+
+    assert len(json.dumps(policy_document, separators=(",", ":"))) == 6144
+
+    errors = list(rule.validate(validator, {}, resource, {}))
+    size_errors = [
+        error
+        for error in errors
+        if error.rule.id == "E3033"
+        and list(error.path) == ["Properties", "PolicyDocument"]
+    ]
+
+    assert size_errors == []
