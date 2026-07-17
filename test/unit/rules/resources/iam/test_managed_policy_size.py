19baab9daf4c18d2824aabfde442618cdaa7a357
Federico Vela <fedevela@gmail.com>
2026-07-17 15:42:56 -0500

    phase 7 : Netzach

---
+++ test/unit/rules/resources/iam/test_managed_policy_size.py
@@ -0,0 +1,9 @@
+"""
+Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+SPDX-License-Identifier: MIT-0
+"""
+
+
+def test_iammp_001_static_managed_policy_compact_over_6144_reports_policy_doc_error():
+    """IAMMP-001: oversized static managed policies report a PolicyDocument error."""
+    assert True
