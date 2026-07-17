3d57087ce9bb060496b95d97da6bb251de5eaec6
Federico Vela <fedevela@gmail.com>
2026-07-17 16:09:17 -0500

    phase 10 : Malkhut

--- test/unit/rules/resources/iam/test_managed_policy_size.py
+++ test/unit/rules/resources/iam/test_managed_policy_size.py
@@ -76,6 +76,36 @@ def test_iammp_002_validating_compact_managed_policy_at_6144_does_not_report_siz
 
 
 def test_iammp_003_validating_compact_managed_policy_below_6144_does_not_report_size_error(
+    validator,
 ):
     """IAMMP-003: a below-limit compact PolicyDocument has no size-limit error."""
-    assert True
+    rule = Properties()
+    rule.child_rules["E3033"] = StringLength()
+    policy_document = {
+        "Version": "2012-10-17",
+        "Statement": [
+            {
+                "Effect": "Allow",
+                "Action": "service:Action",
+                "Resource": "",
+            }
+        ],
+    }
+    compact_length = len(json.dumps(policy_document, separators=(",", ":")))
+    policy_document["Statement"][0]["Resource"] = "a" * (6143 - compact_length)
+    resource = {
+        "Type": "AWS::IAM::ManagedPolicy",
+        "Properties": {"PolicyDocument": policy_document},
+    }
+
+    assert len(json.dumps(policy_document, separators=(",", ":"))) == 6143
+
+    errors = list(rule.validate(validator, {}, resource, {}))
+    size_errors = [
+        error
+        for error in errors
+        if error.rule.id == "E3033"
+        and list(error.path) == ["Properties", "PolicyDocument"]
+    ]
+
+    assert size_errors == []
